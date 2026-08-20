557e95f9db25078979d1286ac007b7553e926a96
antisuck <helloworlddimash@gmail.com>
2026-08-20 20:39:01 +0500

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,21 @@
+local BASE = "https://raw.githubusercontent.com/antisuck/TESTT/main/"
+
+local ESP = loadstring(game:HttpGet(
+    BASE .. "ESP.lua"
+))()
+
+local Combat = loadstring(game:HttpGet(
+    BASE .. "Combat.lua"
+))()
+
+local Misc = loadstring(game:HttpGet(
+    BASE .. "Misc.lua"
+))()
+
+local Cursor = loadstring(game:HttpGet(
+    BASE .. "Cursor.lua"
+))()
+
+local Config = loadstring(game:HttpGet(
+    BASE .. "Config.lua"
+))()
